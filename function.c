7558ba5133271d3e4c0f7d0cb117f0dd7dcca61c
hemarydem <43240309+hemarydem@users.noreply.github.com>
2022-03-29 10:37:31 +0200

    add functions filesé

---
+++ function.c
@@ -0,0 +1,5 @@
+#include "function.h"
+
+char * strrev(char *string) {
+  return string; // reverse the string in place and return it
+}
---
+++ function.h
@@ -0,0 +1,10 @@
+
+#ifndef myQrCodeFunction_h
+#define myQrCodeFunction_h
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char * strrev(char *string)
+#endif
